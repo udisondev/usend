4fecc477037a7f962f245ea84f64f68e9336d981
Udison <udison.myself@gmail.com>
2025-03-03 06:57:48 +0300

    work with verify

--- internal/network/do_verify.go
+++ internal/network/do_verify.go
@@ -1,7 +1,63 @@
 package network
 
-import "udisend/model"
+import (
+	"fmt"
+	"udisend/model"
+	"udisend/pkg/logger"
+	"udisend/pkg/span"
+)
 
-func DoVerify(n *Network, s model.NetworkSignal) {
+func doVerify(n *Network, in model.IncomeSignal) {
+	ctx := span.Init(fmt.Sprintf("node.doVerify user=%s", in.From))
+	logger.Debugf(ctx, "Start...")
+
+	authKey := n.cluster.MemberAuthKey(in.From)
+	if authKey != nil {
+		logger.Debugf(ctx, "Has no member=%s in my cluster", in.From)
+		return
+	}
+
+	challenge := n.challenger.Challenge(in.From, authKey)
+
+	n.send(
+		in.From,
+		model.NetworkSignal{
+			Type:    model.SolveChallengeSignal,
+			Payload: challenge,
+		},
+	)
+
+	logger.Debugf(ctx, "...End")
+}
+
+func newConnection(n *Network, s model.IncomeSignal) {
+
+}
+
+func generateConnection(n *Network, s model.IncomeSignal) {
+
+}
+
+func makeOffer(n *Network, s model.IncomeSignal) {
+
+}
+
+func handleOffer(n *Network, s model.IncomeSignal) {
+
+}
+
+func handleAnswer(n *Network, s model.IncomeSignal) {
+
+}
+
+func connectionEstablished(n *Network, s model.IncomeSignal) {
+
+}
+
+func ping(n *Network, s model.IncomeSignal) {
+
+}
+
+func pong(n *Network, s model.IncomeSignal) {
 
 }
